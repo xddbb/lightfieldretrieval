884d7b187ad9c7bdbcb05c785b637ba892539418
enci.err <enci.err@8409d1d8-b11d-11de-9d1a-45ab25924541>
2009-10-09 20:19:35 +0000

    Single image processing

--- DescriptionExtractor/MainWindow.Designer.cs
+++ DescriptionExtractor/MainWindow.Designer.cs
@@ -28,21 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.imageBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // imageBox
+            // 
+            this.imageBox.Location = new System.Drawing.Point(0, 0);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(256, 256);
+            this.imageBox.TabIndex = 0;
+            this.imageBox.TabStop = false;
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(256, 290);
+            this.Controls.Add(this.imageBox);
             this.Name = "MainWindow";
-            this.Text = "Form1";
+            this.Text = "Feature Extractor";
             this.Load += new System.EventHandler(this.MainWindow_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox imageBox;
     }
 }
 
--- DescriptionExtractor/MainWindow.cs
+++ DescriptionExtractor/MainWindow.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.IO;
+using System.Xml.Serialization;
 
 namespace DescriptionExtractor
 {
     public partial class MainWindow : Form
     {
+        Bitmap image;
+        ZernikeDesc zernike;
+        FeatureVector featureVector;
+
         public MainWindow()
         {
             InitializeComponent();            
@@ -19,7 +24,9 @@ namespace DescriptionExtractor
 
         private void MainWindow_Load(object sender, EventArgs e)
         {
+            /////////////////////////////////////////////////////////////////////////////////
             // Process input
+            /////////////////////////////////////////////////////////////////////////////////
             String[] args = Environment.GetCommandLineArgs();
             if (args.Length < 2)
             {
@@ -31,17 +38,39 @@ namespace DescriptionExtractor
             String filename = args[1];
             if(!File.Exists(filename))
             {
-                MessageBox.Show("No input image(s) provided!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Input image(s) does not exist!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 Application.Exit();
                 return;
             }
             //
             try
             {
-            // Bitmap = 
-            // File file 
+                image = new Bitmap(filename);
+                imageBox.Image = image;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error reding image(s)!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
 
-            int g = 0;
+            /////////////////////////////////////////////////////////////////////////////////
+            // Extract features
+            /////////////////////////////////////////////////////////////////////////////////
+            featureVector = new FeatureVector();
+            zernike = new ZernikeDesc(image);
+            featureVector.zernike = zernike.Process();
+
+
+            /////////////////////////////////////////////////////////////////////////////////
+            // Save to file
+            /////////////////////////////////////////////////////////////////////////////////
+            // Serialization
+            XmlSerializer s = new XmlSerializer(typeof(FeatureVector));
+            FileInfo fileinfo = new FileInfo(filename);
+            TextWriter w = new StreamWriter(fileinfo.Directory + @"\features.xml");
+            s.Serialize(w, featureVector);
+            w.Close();
         }
     }
 }
