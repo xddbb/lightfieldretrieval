d2b3ab5b075601ada4b70c3c8495e975d0644b9c
enci.err <enci.err@8409d1d8-b11d-11de-9d1a-45ab25924541>
2009-10-17 12:16:55 +0000

    Reverted

--- Lightfieldretrieval/Program.cs
+++ Lightfieldretrieval/Program.cs
@@ -1,7 +1,6 @@
 using System;
-using System.Collections;
-using System.IO;
-using DescriptionExtractor;
+
+// NOTE: Reverted single flie
 
 namespace Lightfieldretrieval
 {
@@ -12,38 +11,10 @@ namespace Lightfieldretrieval
         /// </summary>
         static void Main(string[] args)
         {
-            if (args.Length < 1)
-            {
-                return;
-            }
-
-            // Process input directory
-            String dirname = args[0];
-            if(!Directory.Exists(dirname))
-            {
-                return;
-            }
-
-            // Check if file exists
-            if (!File.Exists(dirname + "/basenames"))
-            {
-                return;
-            }
-
-            // Get list of all directory and model files
-            BaseReader reader = new BaseReader(dirname, "basenames");
-
-            int i = 0;
-            foreach( DictionaryEntry de in reader.dirs ){
-                //if (i == 0)
-                {
-                    using (Renderer game = new Renderer())
-                    {
-                        game.filename = de.Key + "/" + de.Value;
-                        game.Run();
-                    }
-                }
-                i++;
+            using (Renderer game = new Renderer())
+            {                
+                game.filename = args[0];
+                game.Run();
             }
         }
     }
